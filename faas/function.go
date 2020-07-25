c13a921a635ce2f7985f7ac05d9c90ff20be7152
Niklas Rosencrantz <niklasr@protonmail.com>
2020-07-25 17:04:06 +0200

    Update function.go

--- faas/function.go
+++ faas/function.go
@@ -9,17 +9,17 @@ import (
 )
 
 // HelloWorld prints the JSON encoded "message" field in the body
-// of the request or "Hello, World!" if there isn't one.
+// of the request or "Hello KoolWorld!" if there isn't one.
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
 	var d struct {
 		Message string `json:"message"`
 	}
 	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
-		fmt.Fprint(w, "Hello World420!")
+		fmt.Fprint(w, "Hello KoolWorld!")
 		return
 	}
 	if d.Message == "" {
-		fmt.Fprint(w, "Hello World420!")
+		fmt.Fprint(w, "Hello KoolWorld!")
 		return
 	}
 	fmt.Fprint(w, html.EscapeString(d.Message))
